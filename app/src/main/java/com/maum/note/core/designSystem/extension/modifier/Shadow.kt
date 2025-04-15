7abe1d1b5beb25b5da5d4d941490ba84fa573416
easyhz <jiton3@naver.com>
2025-04-15 20:47:21 +0900

    feat: Shadow.kt 추가

---
+++ app/src/main/java/com/maum/note/core/designSystem/extension/modifier/Shadow.kt
@@ -0,0 +1,52 @@
+package com.maum.note.core.designSystem.extension.modifier
+
+import android.graphics.BlurMaskFilter
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Figma DropShadow
+ *
+ * @param shape : 그림자의 모양 지정
+ * @param color : 그림자의 색 지정
+ * @param blur : 그림자의 세기 지정
+ * @param offsetY : Y 축 offset 지정
+ * @param offsetX : X 축 offset 지정
+ * @param spread : 그림자의 퍼진 정도 지정
+ */
+fun Modifier.dropShadow(
+    shape: Shape,
+    color: Color = Color.Black.copy(0.25f),
+    blur: Dp = 4.dp,
+    offsetY: Dp = 4.dp,
+    offsetX: Dp = 0.dp,
+    spread: Dp = 0.dp
+) = this.drawBehind {
+    val shadowSize = Size(size.width + spread.toPx(), size.height + spread.toPx())
+    val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
+
+    val paint = Paint().apply {
+        this.color = color
+    }
+
+    if (blur.toPx() > 0) {
+        paint.asFrameworkPaint().apply {
+            maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
+        }
+    }
+
+    drawIntoCanvas { canvas ->
+        canvas.save()
+        canvas.translate(offsetX.toPx(), offsetY.toPx())
+        canvas.drawOutline(shadowOutline, paint)
+        canvas.restore()
+    }
+}
