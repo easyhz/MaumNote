4dadecf1d900d42a8394403cc7e902f1fc0c2b25
easyhz <jiton3@naver.com>
2025-04-15 19:19:59 +0900

    chore: theme 추가

--- app/src/main/java/com/maum/note/ui/theme/Color.kt
+++ app/src/main/java/com/maum/note/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.maum.note.ui.theme
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+@Stable
+val Primary = Color(0xFF1891FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+@Stable
+val MainBackground = Color(0xFFF7F7FB)
+
+@Stable
+val MainText = Color(0xFF232323)
+
+@Stable
+val SubText = Color(0xFF656565)
