7e24bc26ed536d59cc7575e7887d1b1dfd4be8b0
easyhz <jiton3@naver.com>
2025-04-16 18:33:46 +0900

    feat: model 추가

---
+++ app/src/main/java/com/maum/note/data/note/model/request/NoteRequestData.kt
@@ -0,0 +1,5 @@
+package com.maum.note.data.note.model.request
+
+data class NoteRequestData(
+    val content: String,
+)
---
+++ app/src/main/java/com/maum/note/domain/note/model/request/NoteRequest.kt
@@ -0,0 +1,5 @@
+package com.maum.note.domain.note.model.request
+
+data class NoteRequest(
+    val content: String
+)
