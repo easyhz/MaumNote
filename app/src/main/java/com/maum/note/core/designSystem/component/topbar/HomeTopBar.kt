e7309f1944766b6c53dcab17299e046b1a2ef3d2
easyhz <jiton3@naver.com>
2025-04-17 23:22:18 +0900

    fix: HomeTopBar 변경

--- app/src/main/java/com/maum/note/core/designSystem/component/topbar/HomeTopBar.kt
+++ app/src/main/java/com/maum/note/core/designSystem/component/topbar/HomeTopBar.kt
@@ -1,5 +1,6 @@
 package com.maum.note.core.designSystem.component.topbar
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,8 +31,14 @@ fun HomeTopBar(
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Row(
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            Image(
+                modifier = Modifier.height(32.dp),
+                painter = painterResource(R.drawable.ic_app_icon),
+                contentDescription = null
+            )
             Text(
                 modifier = Modifier.widthIn(max = 200.dp),
                 text = stringResource(R.string.app_name),
