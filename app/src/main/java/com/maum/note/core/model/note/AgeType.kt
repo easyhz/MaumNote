499d0278c38065f51e443a5931f447c63ba033cd
easyhz <jiton3@naver.com>
2025-04-18 23:29:14 +0900

    feat: AppType 문자열 추가

--- app/src/main/java/com/maum/note/core/model/note/AgeType.kt
+++ app/src/main/java/com/maum/note/core/model/note/AgeType.kt
@@ -1,8 +1,28 @@
 package com.maum.note.core.model.note
 
-enum class AgeType {
-    NONE,
-    MIXED;
+import androidx.annotation.StringRes
+import com.maum.note.R
+
+enum class AgeType(
+    @StringRes val title: Int
+) {
+    NONE(
+        title = R.string.age_type_none
+    ), MIXED(
+        title = R.string.age_type_mixed
+    ), ZERO(
+        title = R.string.age_type_zero
+    ), ONE(
+        title = R.string.age_type_one
+    ), TWO(
+        title = R.string.age_type_two
+    ), THREE(
+        title = R.string.age_type_three
+    ), FOUR(
+        title = R.string.age_type_four
+    ), FIVE(
+        title = R.string.age_type_five
+    );
 
     companion object {
         fun getByValue(value: String): AgeType? {
