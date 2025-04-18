efd29ecc389cc1465e05dffb4fe1d99e79fa5820
easyhz <jiton3@naver.com>
2025-04-18 12:51:53 +0900

    feat: EmptyView 추가

---
+++ app/src/main/java/com/maum/note/core/designSystem/component/empty/EmptyView.kt
@@ -0,0 +1,35 @@
+package com.maum.note.core.designSystem.component.empty
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.maum.note.ui.theme.SubText
+import com.maum.note.ui.theme.Typography
+
+@Composable
+fun EmptyView(
+    modifier: Modifier = Modifier,
+    text: String = stringResource(id = com.maum.note.R.string.note_empty),
+) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = Typography.body2.copy(
+                color = SubText
+            ),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun EmptyViewPreview() {
+    EmptyView()
+}
