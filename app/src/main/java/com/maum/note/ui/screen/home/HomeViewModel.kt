b780f2d35721596089bf46ca2007f14001ff4f95
easyhz <jiton3@naver.com>
2025-04-18 15:33:22 +0900

    refactor: import 리팩토링

--- app/src/main/java/com/maum/note/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/com/maum/note/ui/screen/home/HomeViewModel.kt
@@ -1,10 +1,10 @@
 package com.maum.note.ui.screen.home
 
 import com.maum.note.core.common.base.BaseViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import com.maum.note.ui.screen.home.contract.HomeSideEffect
 import com.maum.note.ui.screen.home.contract.HomeState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 /**
  * Date: 2025. 4. 15.
