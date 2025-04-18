916518514d5ba28452826194a5a3d220911d0b89
easyhz <jiton3@naver.com>
2025-04-18 11:24:29 +0900

    feat: AppError 추가

---
+++ app/src/main/java/com/maum/note/core/common/error/AppError.kt
@@ -0,0 +1,31 @@
+package com.maum.note.core.common.error
+
+
+sealed class AppError: Exception() {
+    /* 예상하지 못한 에러 */
+    data object UnexpectedError : AppError() {
+        @JvmStatic
+        private fun readResolve(): Any = UnexpectedError
+    }
+
+    /* 결과값 반환 X */
+    data object NoResultError : AppError() {
+        @JvmStatic
+        private fun readResolve(): Any = NoResultError
+    }
+
+    /* 인덱스 에러 */
+    data object IndexError : AppError() {
+        @JvmStatic
+        private fun readResolve(): Any = IndexError
+    }
+
+    /* 유저 데이터 없음 에러 */
+    data object NoUserDataError : AppError() {
+        @JvmStatic
+        private fun readResolve(): Any = NoUserDataError
+    }
+
+    data class DefaultError(override val message: String) : AppError()
+
+}
