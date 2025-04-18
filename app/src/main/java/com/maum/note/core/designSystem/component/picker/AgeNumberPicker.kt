58fbca3847e2249ea169c063705b8b3bc8c8bdca
easyhz <jiton3@naver.com>
2025-04-18 23:30:48 +0900

    feat: AgeNumberPicker 추가

---
+++ app/src/main/java/com/maum/note/core/designSystem/component/picker/AgeNumberPicker.kt
@@ -0,0 +1,45 @@
+package com.maum.note.core.designSystem.component.picker
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.maum.note.core.designSystem.util.picker.PickerState
+import com.maum.note.core.designSystem.util.picker.getItem
+import com.maum.note.core.designSystem.util.picker.rememberPickerState
+import com.maum.note.core.model.note.AgeType
+import com.maum.note.ui.theme.AppTypography
+
+@Composable
+fun AgeNumberPicker(
+    modifier: Modifier = Modifier,
+    state: PickerState<AgeType>,
+    values: List<AgeType> = AgeType.entries,
+) {
+    Picker(
+        modifier = modifier,
+        state = state,
+        items = values,
+    ) { contentModifier, index ->
+        Text(
+            modifier = contentModifier.padding(8.dp),
+            text = stringResource(getItem(values, index).title),
+            style = AppTypography.body2_semiBold,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    val valuesPickerState = rememberPickerState(AgeType.NONE)
+    AgeNumberPicker(
+        state = valuesPickerState,
+    )
+}
