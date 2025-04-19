b53c0d2a03c6f67059522965744f2c2026081070
easyhz <jiton3@naver.com>
2025-04-20 00:00:24 +0900

    feat: NoteDetailType 추가

---
+++ app/src/main/java/com/maum/note/core/model/note/NoteDetailType.kt
@@ -0,0 +1,55 @@
+package com.maum.note.core.model.note
+
+import androidx.annotation.StringRes
+import com.maum.note.R
+
+sealed class NoteDetailType(
+    @StringRes val title: Int,
+    val content: String,
+) {
+    data class Type(val noteType: NoteType, val value: String): NoteDetailType(
+        title = noteType.title,
+        content = value,
+    )
+    data class Age(
+        val value: String
+    ): NoteDetailType(
+        title = R.string.note_detail_age,
+        content = value,
+    )
+    data class Sentence(
+        val value: String
+    ): NoteDetailType(
+        title = R.string.note_detail_sentence,
+        content = value,
+    )
+    data class Input(
+        val value: String
+    ): NoteDetailType(
+        title = R.string.note_detail_input,
+        content = value,
+    )
+
+    companion object {
+        fun entries(
+            noteType: NoteType,
+            typeValue: String,
+            ageValue: String,
+            sentenceValue: String,
+            inputValue: String,
+        ) = listOf(
+            Type(noteType, typeValue),
+            Age(ageValue),
+            Sentence(sentenceValue),
+            Input(inputValue),
+        )
+
+        val empty = entries(
+            noteType = NoteType.DEFAULT,
+            typeValue = "",
+            ageValue = "",
+            sentenceValue = "",
+            inputValue = "",
+        )
+    }
+}
