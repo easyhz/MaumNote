ef9f678e5689155bcf53d6a30756c24b376a18f3
easyhz <jiton3@naver.com>
2025-04-22 00:04:47 +0900

    fix: 이름 변경

--- app/src/main/java/com/maum/note/domain/note/model/request/NoteRequestParam.kt
+++ app/src/main/java/com/maum/note/domain/note/model/request/NoteRequestParam.kt
@@ -1,6 +1,6 @@
 package com.maum.note.domain.note.model.request
 
-data class NoteRequest(
+data class NoteRequestParam(
     val noteType: String,
     val age: String,
     val sentenceCount: String,
