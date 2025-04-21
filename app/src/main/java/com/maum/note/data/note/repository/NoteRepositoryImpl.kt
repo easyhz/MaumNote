6c5b66274994761704169303518a6ff414b1a9dc
easyhz <jiton3@naver.com>
2025-04-22 00:06:06 +0900

    refactor: 파라미터명 변경

--- app/src/main/java/com/maum/note/data/note/repository/NoteRepositoryImpl.kt
+++ app/src/main/java/com/maum/note/data/note/repository/NoteRepositoryImpl.kt
@@ -2,14 +2,14 @@ package com.maum.note.data.note.repository
 
 import com.maum.note.data.note.datasource.local.NoteLocalDataSource
 import com.maum.note.data.note.mapper.toRequestData
-import com.maum.note.domain.note.model.request.NoteRequest
+import com.maum.note.domain.note.model.request.NoteRequestParam
 import com.maum.note.domain.note.repository.NoteRepository
 import javax.inject.Inject
 
 class NoteRepositoryImpl @Inject constructor(
     private val noteLocalDataSource: NoteLocalDataSource,
 ): NoteRepository {
-    override suspend fun saveNote(request: NoteRequest) {
+    override suspend fun saveNote(request: NoteRequestParam) {
         noteLocalDataSource.saveNote(
             data = request.toRequestData()
         )
--- app/src/main/java/com/maum/note/domain/note/repository/NoteRepository.kt
+++ app/src/main/java/com/maum/note/domain/note/repository/NoteRepository.kt
@@ -1,7 +1,7 @@
 package com.maum.note.domain.note.repository
 
-import com.maum.note.domain.note.model.request.NoteRequest
+import com.maum.note.domain.note.model.request.NoteRequestParam
 
 interface NoteRepository {
-    suspend fun saveNote(request: NoteRequest)
+    suspend fun saveNote(request: NoteRequestParam)
 }
