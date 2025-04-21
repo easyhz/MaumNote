27e64b1e03d5efc6091546fe3fa71f0e27143425
easyhz <jiton3@naver.com>
2025-04-22 00:08:08 +0900

    feat: ServiceModule 추가

---
+++ app/src/main/java/com/maum/note/core/firebase/di/ServiceModule.kt
@@ -0,0 +1,18 @@
+package com.maum.note.core.firebase.di
+
+import com.maum.note.core.firebase.user.service.UserService
+import com.maum.note.core.firebase.user.service.UserServiceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface ServiceModule {
+    @Binds
+    fun bindUserService(
+        userServiceImpl: UserServiceImpl
+    ): UserService
+}
