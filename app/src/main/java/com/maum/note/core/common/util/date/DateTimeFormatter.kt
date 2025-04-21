9e451bf80ee840622a894abe25c6b55d0b79ccb3
easyhz <jiton3@naver.com>
2025-04-22 00:06:52 +0900

    feat: DateTimeFormatter 메서드 추가

--- app/src/main/java/com/maum/note/core/common/util/date/DateTimeFormatter.kt
+++ app/src/main/java/com/maum/note/core/common/util/date/DateTimeFormatter.kt
@@ -1,7 +1,10 @@
 package com.maum.note.core.common.util.date
 
+import com.google.firebase.Timestamp
 import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.util.Date
 
 class DateTimeFormatter {
     private val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm")
@@ -12,5 +15,14 @@ class DateTimeFormatter {
 
     fun convertStringToDateTime(dateTimeString: String): LocalDateTime {
         return LocalDateTime.parse(dateTimeString, formatter)
+    }
+
+    fun formatTimestampToDateTime(timestamp: Timestamp): LocalDateTime {
+        return LocalDateTime.ofInstant(timestamp.toDate().toInstant(), ZoneId.systemDefault())
+    }
+
+    fun formatLocalDateTimeToTimestamp(localDateTime: LocalDateTime): Timestamp {
+        val instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant()
+        return Timestamp(Date.from(instant))
     }
 }
