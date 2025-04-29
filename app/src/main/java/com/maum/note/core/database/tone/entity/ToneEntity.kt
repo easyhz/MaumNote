4394860f47e03e664f8b140b4a72fb36501e2fbe
easyhz <jiton3@naver.com>
2025-04-29 17:54:46 +0900

    feat: ToneEntity 추가

---
+++ app/src/main/java/com/maum/note/core/database/tone/entity/ToneEntity.kt
@@ -0,0 +1,27 @@
+package com.maum.note.core.database.tone.entity
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+/**
+ * @param id
+ * @param toneType [ToneType] name - 톤 종류
+ * @param content - 내용
+ * @param isSelected - 선택 여부
+ * @param createdAt - 생성일
+ * @param updatedAt - 수정일
+ */
+@Entity(
+    tableName = "TONE",
+    indices = [Index(value = ["toneType", "isSelected"], unique = true)]
+)
+data class ToneEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val toneType: String, // ToneType
+    val content: String,
+    val isSelected: Boolean,
+    val createdAt: String, // LocalDateTime
+    val updatedAt: String, // LocalDateTime
+)
