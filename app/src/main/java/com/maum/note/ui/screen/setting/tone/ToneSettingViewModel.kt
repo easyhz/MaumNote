7a24409319b2c4e77e5de3bc9300ca19cc8da2be
easyhz <jiton3@naver.com>
2025-04-30 19:11:20 +0900

    feat: updateAllTone 로직 추가

--- app/src/main/java/com/maum/note/ui/screen/setting/tone/ToneSettingViewModel.kt
+++ app/src/main/java/com/maum/note/ui/screen/setting/tone/ToneSettingViewModel.kt
@@ -6,13 +6,16 @@ import com.maum.note.core.common.base.BaseViewModel
 import com.maum.note.core.common.di.dispatcher.AppDispatchers
 import com.maum.note.core.common.di.dispatcher.Dispatcher
 import com.maum.note.core.model.note.NoteType
+import com.maum.note.domain.tone.model.request.UpdateToneRequestParam
 import com.maum.note.domain.tone.usecase.GetAllSelectedTonesUseCase
+import com.maum.note.domain.tone.usecase.UpdateAllToneUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import com.maum.note.ui.screen.setting.tone.contract.ToneSettingSideEffect
 import com.maum.note.ui.screen.setting.tone.contract.ToneSettingState
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * Date: 2025. 4. 19.
@@ -23,7 +26,8 @@ import kotlinx.coroutines.launch
 class ToneSettingViewModel @Inject constructor(
     @Dispatcher(AppDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
     @Dispatcher(AppDispatchers.MAIN) private val mainDispatcher: CoroutineDispatcher,
-    private val getAllSelectedTonesUseCase: GetAllSelectedTonesUseCase
+    private val getAllSelectedTonesUseCase: GetAllSelectedTonesUseCase,
+    private val updateAllToneUseCase: UpdateAllToneUseCase,
 ) : BaseViewModel<ToneSettingState, ToneSettingSideEffect>(
     initialState = ToneSettingState.init()
 ) {
@@ -44,7 +48,7 @@ class ToneSettingViewModel @Inject constructor(
     }
 
     fun onClickSave() {
-
+        updateAllTone()
     }
 
     private fun getAllSelectedTones() {
@@ -55,10 +59,39 @@ class ToneSettingViewModel @Inject constructor(
                         type to TextFieldValue(tone.content)
                     }
                 }.toMap()
-                setState { copy(contents = contents) }
+                withContext(mainDispatcher) {
+                    setState { copy(contents = contents) }
+                }
             }.onFailure {
-                setState { copy(isLoading = false) }
+                withContext(mainDispatcher) {
+                    setState { copy(isLoading = false) }
+                }
             }
+        }
+    }
+
+    private fun updateAllTone() {
+        viewModelScope.launch(ioDispatcher) {
+            val param = getUpdateToneRequestParam()
+            updateAllToneUseCase.invoke(param).onSuccess {
+                withContext(mainDispatcher) {
+                    setState { copy(isLoading = false) }
+                }
+            }.onFailure {
+                withContext(mainDispatcher) {
+                    setState { copy(isLoading = false) }
+                    // TODO SNACKBAR
+                }
+            }
+        }
+    }
+
+    private fun getUpdateToneRequestParam(): List<UpdateToneRequestParam> {
+        return currentState.contents.map { (noteType, textFieldValue) ->
+            UpdateToneRequestParam(
+                noteType = noteType.name,
+                content = textFieldValue.text
+            )
         }
     }
 }
