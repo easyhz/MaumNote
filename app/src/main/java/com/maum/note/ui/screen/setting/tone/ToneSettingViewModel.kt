311a16f8b19dc7dd82fcf967562b440cec1b82d5
easyhz <jiton3@naver.com>
2025-04-30 18:49:43 +0900

    feat: getAllSelectedTonesUseCase 로직 추가

--- app/src/main/java/com/maum/note/ui/screen/setting/tone/ToneSettingViewModel.kt
+++ app/src/main/java/com/maum/note/ui/screen/setting/tone/ToneSettingViewModel.kt
@@ -1,12 +1,18 @@
 package com.maum.note.ui.screen.setting.tone
 
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.lifecycle.viewModelScope
 import com.maum.note.core.common.base.BaseViewModel
+import com.maum.note.core.common.di.dispatcher.AppDispatchers
+import com.maum.note.core.common.di.dispatcher.Dispatcher
 import com.maum.note.core.model.note.NoteType
+import com.maum.note.domain.tone.usecase.GetAllSelectedTonesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import com.maum.note.ui.screen.setting.tone.contract.ToneSettingSideEffect
 import com.maum.note.ui.screen.setting.tone.contract.ToneSettingState
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
 
 /**
  * Date: 2025. 4. 19.
@@ -15,10 +21,20 @@ import com.maum.note.ui.screen.setting.tone.contract.ToneSettingState
 
 @HiltViewModel
 class ToneSettingViewModel @Inject constructor(
-
+    @Dispatcher(AppDispatchers.IO) private val ioDispatcher: CoroutineDispatcher,
+    @Dispatcher(AppDispatchers.MAIN) private val mainDispatcher: CoroutineDispatcher,
+    private val getAllSelectedTonesUseCase: GetAllSelectedTonesUseCase
 ) : BaseViewModel<ToneSettingState, ToneSettingSideEffect>(
     initialState = ToneSettingState.init()
 ) {
+
+    init {
+        init()
+    }
+
+    private fun init() {
+        getAllSelectedTones()
+    }
 
     fun onContentValueChange(
         noteType: NoteType,
@@ -29,5 +45,20 @@ class ToneSettingViewModel @Inject constructor(
 
     fun onClickSave() {
 
+    }
+
+    private fun getAllSelectedTones() {
+        viewModelScope.launch(ioDispatcher) {
+            getAllSelectedTonesUseCase.invoke(Unit).onSuccess { tones ->
+                val contents = tones.mapNotNull { tone ->
+                    NoteType.getByValue(tone.noteType)?.let { type ->
+                        type to TextFieldValue(tone.content)
+                    }
+                }.toMap()
+                setState { copy(contents = contents) }
+            }.onFailure {
+                setState { copy(isLoading = false) }
+            }
+        }
     }
 }
