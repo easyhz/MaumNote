cb4b37e3989f926c11d9041add10d03b302f8415
easyhz <jiton3@naver.com>
2025-05-02 23:15:47 +0900

    feat: HttpError 추가

---
+++ app/src/main/java/com/maum/note/core/common/error/HttpError.kt
@@ -0,0 +1,38 @@
+package com.maum.note.core.common.error
+
+import com.maum.note.core.network.common.model.error.gpt.GptErrorResponse
+
+sealed class HttpError : AppError() {
+
+    /**
+     * 400 Bad Request
+     */
+    data class BadRequestError(override val message: String) : HttpError()
+
+    /**
+     * 401 Unauthorized
+     */
+    data class UnauthorizedError(override val message: String) : HttpError()
+
+    /**
+     * 403 Forbidden
+     */
+    data class ForbiddenError(override val message: String) : HttpError()
+
+    /**
+     * 404 Not Found
+     */
+    data class NotFoundError(override val message: String) : HttpError()
+
+    /**
+     * 500 Internal Server Error
+     */
+    data class InternalServerError(override val message: String) : HttpError()
+}
+
+fun getGptError(error: GptErrorResponse): AppError {
+    return AppError.GptRequestError(
+        message = error.error.message,
+        error = error.error
+    )
+}
