109d5cdbc92db289958676c59cd17ae317258bcd
easyhz <jiton3@naver.com>
2025-05-05 14:04:44 +0900

    feat: 직렬화 Helper 추가

---
+++ app/src/main/java/com/maum/note/core/common/helper/serializable/LocalDateTimeAdapter.kt
@@ -0,0 +1,20 @@
+package com.maum.note.core.common.helper.serializable
+
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.ToJson
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class LocalDateTimeAdapter {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
+
+    @ToJson
+    fun toJson(localDateTime: LocalDateTime): String {
+        return localDateTime.format(formatter)
+    }
+
+    @FromJson
+    fun fromJson(dateString: String): LocalDateTime {
+        return LocalDateTime.parse(dateString, formatter)
+    }
+}
---
+++ app/src/main/java/com/maum/note/core/common/helper/serializable/MoshiSerializableHelper.kt
@@ -0,0 +1,42 @@
+package com.maum.note.core.common.helper.serializable
+
+import com.maum.note.core.common.helper.log.Logger
+import com.squareup.moshi.Moshi
+import java.lang.reflect.Type
+import javax.inject.Inject
+
+internal class MoshiSerializableHelper @Inject constructor(
+    private val moshi: Moshi,
+    private val logger: Logger
+): SerializableHelper {
+    private val tag = MoshiSerializableHelper::class.java.simpleName
+
+    override fun <T> serialize(data: T, clazz: Class<T>): String {
+        return moshi.adapter(clazz).toJson(data)
+    }
+
+
+    override fun <T> serialize(data: T, type: Type): String {
+        return moshi.adapter<T>(type).toJson(data)
+    }
+
+    override fun <T> deserialize(data: String?, clazz: Class<T>): T? {
+        try {
+            return if (data.isNullOrBlank()) null
+            else moshi.adapter(clazz).fromJson(data)!!
+        } catch (e: Exception) {
+            logger.e(tag, "deserialize", e)
+            return null
+        }
+    }
+
+    override fun <T> deserialize(data: String?, type: Type): T? {
+        try {
+            return if (data.isNullOrBlank()) null
+            else moshi.adapter<T>(type).fromJson(data)!!
+        } catch (e: Exception) {
+            logger.e(tag, "deserialize", e)
+            return null
+        }
+    }
+}
