b004e7fc806dd366935b51f357f1c78f80cf1d0b
easyhz <jiton3@naver.com>
2025-05-05 14:06:01 +0900

    feat: CreateNoteUseCase 추가

---
+++ app/src/main/java/com/maum/note/domain/note/usecase/CreateNoteUseCase.kt
@@ -0,0 +1,15 @@
+package com.maum.note.domain.note.usecase
+
+import com.maum.note.core.common.base.BaseUseCase
+import com.maum.note.domain.note.model.request.CreateNoteRequestParam
+import com.maum.note.domain.note.model.response.CreateNoteResponse
+import com.maum.note.domain.note.repository.NoteRepository
+import javax.inject.Inject
+
+class CreateNoteUseCase @Inject constructor(
+    private val noteRepository: NoteRepository,
+) : BaseUseCase<CreateNoteRequestParam, CreateNoteResponse>() {
+    override suspend fun invoke(param: CreateNoteRequestParam): Result<CreateNoteResponse> {
+        return noteRepository.createNote(param)
+    }
+}
