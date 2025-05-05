ceecb5c87b2cc99a54d661d82b1171479c801eba
easyhz <jiton3@naver.com>
2025-05-05 14:05:45 +0900

    feat: mapper 수정

--- app/src/main/java/com/maum/note/data/note/mapper/NoteMapper.kt
+++ app/src/main/java/com/maum/note/data/note/mapper/NoteMapper.kt
@@ -1,10 +1,23 @@
 package com.maum.note.data.note.mapper
 
+import com.maum.note.R
+import com.maum.note.core.common.helper.resource.ResourceHelper
+import com.maum.note.core.common.util.gpt.GptRole
 import com.maum.note.core.database.note.entity.NoteEntity
+import com.maum.note.core.model.note.NoteType
+import com.maum.note.core.network.gpt.model.note.request.GptRequest
+import com.maum.note.core.network.gpt.model.note.request.etc.InputRequest
+import com.maum.note.core.network.gpt.model.note.response.GptResponse
+import com.maum.note.data.note.model.CreateNoteMapParam
+import com.maum.note.data.note.model.InputRequestMapParam
 import com.maum.note.domain.note.model.request.NoteRequestParam
+import com.maum.note.domain.note.model.response.CreateNoteResponse
 import java.time.LocalDateTime
+import javax.inject.Inject
 
-class NoteMapper {
+class NoteMapper @Inject constructor(
+    private val resourceHelper: ResourceHelper,
+) {
     fun mapToNoteEntity(noteRequestParam: NoteRequestParam): NoteEntity = NoteEntity(
         noteType = noteRequestParam.noteType,
         age = noteRequestParam.age,
@@ -15,4 +28,70 @@ class NoteMapper {
         updatedAt = LocalDateTime.now().toString(),
         isDeleted = false
     )
+
+    fun mapToCreateNoteRequest(
+        param: CreateNoteMapParam,
+    ): GptRequest =
+        GptRequest(
+            model = resourceHelper.getString(R.string.model),
+            input = createGptInputRequests(
+                param = param.toInputRequestMapParam()
+            )
+        )
+
+    fun mapToCreateNoteResponse(
+        response: GptResponse
+    ) = CreateNoteResponse(
+        status = response.status,
+        result = response.output.getOrNull(0)?.content?.getOrNull(0)?.text ?: "",
+    )
+
+    private fun createGptInputRequests(
+        param: InputRequestMapParam,
+    ): List<InputRequest> {
+        val systemPrompt = InputRequest(
+            role = GptRole.SYSTEM.alias,
+            content = getSystemPrompt(noteType = param.noteType)
+        )
+        val userPrompt = InputRequest(
+            role = GptRole.USER.alias,
+            content = getUserPrompt(
+                param = param
+            )
+        )
+        return listOf(systemPrompt, userPrompt)
+    }
+
+    private fun getSystemPrompt(
+        noteType: NoteType
+    ): String {
+        val id = when (noteType) {
+            NoteType.LETTER_GREETING -> R.string.system_prompt_letter_greeting
+            NoteType.ANNOUNCEMENT_CONTENT -> R.string.system_prompt_announcement_content
+            NoteType.PLAY_CONTEXT -> R.string.system_prompt_play_context
+            else -> R.string.system_prompt_default
+        }
+        return resourceHelper.getString(id)
+    }
+
+    private fun getUserPrompt(
+        param: InputRequestMapParam,
+    ): String {
+        val age = resourceHelper.getString(param.ageType.title)
+        val sentence = resourceHelper.getString(param.sentenceType.title)
+        val note = resourceHelper.getString(param.noteType.title)
+        val inputContent = param.inputContent
+        val defaultTone = param.defaultTone
+        val typeTone = param.typeTone
+
+        return resourceHelper.getString(
+            R.string.user_prompt,
+            age,
+            sentence,
+            inputContent,
+            defaultTone,
+            note,
+            typeTone
+        )
+    }
 }
