0e535cec6c8c64c5d2e9325c91358b92f5657c34
easyhz <jiton3@naver.com>
2025-05-05 14:05:56 +0900

    feat: NoteRemoteDataSource 추가

---
+++ app/src/main/java/com/maum/note/data/note/datasource/remote/NoteRemoteDataSource.kt
@@ -0,0 +1,9 @@
+package com.maum.note.data.note.datasource.remote
+
+import com.maum.note.core.network.gpt.model.note.request.GptRequest
+import com.maum.note.core.network.gpt.model.note.response.GptResponse
+
+
+interface NoteRemoteDataSource {
+    suspend fun createNote(request: GptRequest): Result<GptResponse>
+}
---
+++ app/src/main/java/com/maum/note/data/note/datasource/remote/NoteRemoteDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.maum.note.data.note.datasource.remote
+
+import com.maum.note.core.network.gpt.api.GptApi
+import com.maum.note.core.network.gpt.model.note.request.GptRequest
+import com.maum.note.core.network.gpt.model.note.response.GptResponse
+import javax.inject.Inject
+
+
+class NoteRemoteDataSourceImpl @Inject constructor(
+    private val gptApi: GptApi
+): NoteRemoteDataSource {
+    override suspend fun createNote(request: GptRequest): Result<GptResponse> {
+        return gptApi.requestCreateNote(request)
+    }
+}
