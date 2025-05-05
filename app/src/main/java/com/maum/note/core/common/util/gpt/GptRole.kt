ef3ed058214e1cab06ddd69edfee23055752d852
easyhz <jiton3@naver.com>
2025-05-05 14:06:20 +0900

    feat: GptRole 추가

---
+++ app/src/main/java/com/maum/note/core/common/util/gpt/GptRole.kt
@@ -0,0 +1,12 @@
+package com.maum.note.core.common.util.gpt
+
+enum class GptRole(
+    val alias: String
+) {
+    SYSTEM(
+        alias = "system"
+    ),
+    USER(
+        alias = "user"
+    );
+}
