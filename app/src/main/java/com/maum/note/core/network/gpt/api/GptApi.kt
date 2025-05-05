8979aab8aeb5e396cccb557d41d8200beea4360a
easyhz <jiton3@naver.com>
2025-05-05 20:24:42 +0900

    fix: api 어노테이션 추가

--- app/src/main/java/com/maum/note/core/network/gpt/api/GptApi.kt
+++ app/src/main/java/com/maum/note/core/network/gpt/api/GptApi.kt
@@ -2,9 +2,10 @@ package com.maum.note.core.network.gpt.api
 
 import com.maum.note.core.network.gpt.model.note.request.GptRequest
 import com.maum.note.core.network.gpt.model.note.response.GptResponse
+import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface GptApi {
     @POST("/v1/responses")
-    suspend fun requestNoteGenerate(request: GptRequest): Result<GptResponse>
+    suspend fun requestNoteGenerate(@Body request: GptRequest): Result<GptResponse>
 }
