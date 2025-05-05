9248f31183c27972c523201d7e905ad347c4e980
easyhz <jiton3@naver.com>
2025-05-05 14:06:16 +0900

    feat: SentenceType title 추가

--- app/src/main/java/com/maum/note/core/model/note/SentenceType.kt
+++ app/src/main/java/com/maum/note/core/model/note/SentenceType.kt
@@ -1,8 +1,17 @@
 package com.maum.note.core.model.note
 
-enum class SentenceType {
-    ONE_TO_TWO,
-    THREE_TO_FOUR;
+import androidx.annotation.StringRes
+import com.maum.note.R
+
+enum class SentenceType(
+    @StringRes val title: Int
+) {
+    ONE_TO_TWO(
+        title = R.string.sentence_one_to_two
+    ),
+    THREE_TO_FOUR(
+        title = R.string.sentence_three_to_four
+    );
 
     companion object {
         fun getByValue(value: String): SentenceType? {
