9eb83434f404e69a9b9a62909904e70eb687cd7a
easyhz <jiton3@naver.com>
2025-05-06 22:09:56 +0900

    feat: Home 네비게이션 애니메이션 수정

--- app/src/main/java/com/maum/note/core/navigation/home/HomeGraph.kt
+++ app/src/main/java/com/maum/note/core/navigation/home/HomeGraph.kt
@@ -1,9 +1,15 @@
 package com.maum.note.core.navigation.home
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.maum.note.core.designSystem.util.transition.SlideDirection
+import com.maum.note.core.designSystem.util.transition.enterSlide
+import com.maum.note.core.designSystem.util.transition.exitSlide
 import com.maum.note.core.navigation.note.creation.navigateToNoteCreation
 import com.maum.note.core.navigation.home.screen.Home
 import com.maum.note.core.navigation.note.detail.navigateToNoteDetail
@@ -13,7 +19,12 @@ import com.maum.note.ui.screen.home.HomeScreen
 fun NavGraphBuilder.homeGraph(
     navController: NavController
 ) {
-    composable<Home> {
+    composable<Home>(
+        enterTransition = { fadeIn(animationSpec = tween(300)) },
+        exitTransition = { fadeOut(animationSpec = tween(300)) },
+        popEnterTransition = { fadeIn(animationSpec = tween(300)) },
+        popExitTransition = { exitSlide(SlideDirection.End) }
+    ) {
         HomeScreen(
             navigateToSetting = navController::navigateToSetting,
             navigateToCreation = navController::navigateToNoteCreation,
