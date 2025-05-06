c41abf8358546ad8c85188c61b211dbdd0f04136
easyhz <jiton3@naver.com>
2025-05-06 22:05:08 +0900

    feat: UserStep 추가

--- app/src/main/java/com/maum/note/core/model/user/UserStep.kt
+++ app/src/main/java/com/maum/note/core/model/user/UserStep.kt
@@ -4,4 +4,6 @@ sealed class UserStep {
     data object NewUserToOnboarding : UserStep()
     data object ExistingUserToOnboarding : UserStep()
     data object AlreadyLoginToMain : UserStep()
+    data class Maintenance(val notice: String) : UserStep()
+    data class Update(val newVersion: String) : UserStep()
 }
