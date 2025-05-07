d51d6e073dd91ed00cbccd73cc606ee8374e3219
easyhz <jiton3@naver.com>
2025-05-07 15:02:56 +0900

    feat: AppDateTimeFormatter 받기

--- app/src/main/java/com/maum/note/MainActivity.kt
+++ app/src/main/java/com/maum/note/MainActivity.kt
@@ -5,18 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.maum.note.core.common.util.date.AppDateTimeFormatter
 import com.maum.note.core.navigation.MaumNoteApp
 import com.maum.note.ui.theme.MaumNoteTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var appDateTimeFormatter: AppDateTimeFormatter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
         setContent {
-            MaumNoteTheme {
+            MaumNoteTheme(
+                appDateTimeFormatter = appDateTimeFormatter
+            ) {
                 MaumNoteApp()
             }
         }
--- app/src/main/java/com/maum/note/ui/theme/Theme.kt
+++ app/src/main/java/com/maum/note/ui/theme/Theme.kt
@@ -3,15 +3,25 @@ package com.maum.note.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
+import com.maum.note.core.common.util.date.AppDateTimeFormatter
 
 val LocalSnackBarHostState = staticCompositionLocalOf { SnackbarHostState() }
+val LocalDateTimeFormatter = staticCompositionLocalOf<AppDateTimeFormatter> {
+    error("No DateTimeFormatter provided")
+}
 
 @Composable
 fun MaumNoteTheme(
+    appDateTimeFormatter: AppDateTimeFormatter = AppDateTimeFormatter(),
     content: @Composable () -> Unit
 ) {
-    MaterialTheme(
-        content = content
-    )
+    CompositionLocalProvider(
+        LocalDateTimeFormatter provides appDateTimeFormatter
+    ) {
+        MaterialTheme(
+            content = content
+        )
+    }
 }
