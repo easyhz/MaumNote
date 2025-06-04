4ecf5637b8f6b4749224b8e4a267e989b7c096de
easyhz <jiton3@naver.com>
2025-06-05 01:06:44 +0900

    feat: service di 추가

---
+++ app/src/main/java/com/maum/note/core/supabase/di/ServiceModule.kt
@@ -0,0 +1,24 @@
+package com.maum.note.core.supabase.di
+
+import com.maum.note.core.supabase.user.service.UserService
+import com.maum.note.core.supabase.user.service.UserServiceImpl
+import com.maum.note.core.supabase.auth.AuthService
+import com.maum.note.core.supabase.auth.AuthServiceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface ServiceModule {
+    @Binds
+    fun bindAuthService(
+        authServiceImpl: AuthServiceImpl
+    ): AuthService
+
+    @Binds
+    fun bindUserService(
+        userServiceImpl: UserServiceImpl
+    ): UserService
+}
