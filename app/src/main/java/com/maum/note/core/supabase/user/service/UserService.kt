c6a2106dd56ad17532970645a6eeec46e06882f4
easyhz <jiton3@naver.com>
2025-06-05 03:31:36 +0900

    feat: UserService update 추가

--- app/src/main/java/com/maum/note/core/supabase/user/service/UserService.kt
+++ app/src/main/java/com/maum/note/core/supabase/user/service/UserService.kt
@@ -5,4 +5,8 @@ import com.maum.note.core.supabase.user.dto.UserDto
 interface UserService {
     suspend fun insertUser(userDto: UserDto)
     suspend fun fetchUser(userId: String): UserDto?
+
+    suspend fun updateUserNickname(userId: String, nickname: String)
+    suspend fun updateUserStudentAge(userId: String, ageType: String)
+    suspend fun updateUserHasAgreedToTerms(userId: String, hasAgreedToTerms: Boolean)
 }
--- app/src/main/java/com/maum/note/core/supabase/user/service/UserServiceImpl.kt
+++ app/src/main/java/com/maum/note/core/supabase/user/service/UserServiceImpl.kt
@@ -1,13 +1,13 @@
 package com.maum.note.core.supabase.user.service
 
-import com.maum.note.core.supabase.user.dto.UserDto
 import com.maum.note.core.supabase.constant.Table
+import com.maum.note.core.supabase.user.dto.UserDto
 import io.github.jan.supabase.postgrest.Postgrest
 import javax.inject.Inject
 
 class UserServiceImpl @Inject constructor(
     private val postgrest: Postgrest
-): UserService {
+) : UserService {
     override suspend fun insertUser(userDto: UserDto) {
         postgrest.from(Table.USERS.name).insert(userDto)
     }
@@ -17,6 +17,45 @@ class UserServiceImpl @Inject constructor(
             filter {
                 eq(Table.USERS.ID, userId)
             }
-        }.decodeSingle()
+        }.decodeSingleOrNull()
+    }
+
+    override suspend fun updateUserNickname(userId: String, nickname: String) {
+        postgrest.from(Table.USERS.name).update(
+            {
+                set(Table.USERS.NICKNAME, nickname)
+            }
+        ) {
+            filter {
+                eq(Table.USERS.ID, userId)
+            }
+        }
+    }
+
+    override suspend fun updateUserStudentAge(userId: String, ageType: String) {
+        postgrest.from(Table.USERS.name).update(
+            {
+                set(Table.USERS.STUDENT_AGE, ageType)
+            }
+        ) {
+            filter {
+                eq(Table.USERS.ID, userId)
+            }
+        }
+    }
+
+    override suspend fun updateUserHasAgreedToTerms(
+        userId: String,
+        hasAgreedToTerms: Boolean
+    ) {
+        postgrest.from(Table.USERS.name).update(
+            {
+                set(Table.USERS.HAS_AGREED_TO_TERMS, hasAgreedToTerms)
+            }
+        ) {
+            filter {
+                eq(Table.USERS.ID, userId)
+            }
+        }
     }
 }
