dbb23e88dea7e32b381fbce2ac921c5faf20e233
easyhz <jiton3@naver.com>
2025-06-05 12:37:12 +0900

    feat: add note insertion method to remote data source for note creation support

--- app/src/main/java/com/maum/note/data/note/datasource/remote/NoteRemoteDataSource.kt
+++ app/src/main/java/com/maum/note/data/note/datasource/remote/NoteRemoteDataSource.kt
@@ -2,8 +2,10 @@ package com.maum.note.data.note.datasource.remote
 
 import com.maum.note.core.network.gpt.model.note.request.GptRequest
 import com.maum.note.core.network.gpt.model.note.response.GptResponse
+import com.maum.note.core.supabase.note.dto.NoteDto
 
 
 interface NoteRemoteDataSource {
     suspend fun generateNote(request: GptRequest): Result<GptResponse>
+    suspend fun insertNote(noteDto: NoteDto)
 }
--- app/src/main/java/com/maum/note/data/note/datasource/remote/NoteRemoteDataSourceImpl.kt
+++ app/src/main/java/com/maum/note/data/note/datasource/remote/NoteRemoteDataSourceImpl.kt
@@ -3,13 +3,20 @@ package com.maum.note.data.note.datasource.remote
 import com.maum.note.core.network.gpt.api.GptApi
 import com.maum.note.core.network.gpt.model.note.request.GptRequest
 import com.maum.note.core.network.gpt.model.note.response.GptResponse
+import com.maum.note.core.supabase.note.dto.NoteDto
+import com.maum.note.core.supabase.note.service.NoteService
 import javax.inject.Inject
 
 
 class NoteRemoteDataSourceImpl @Inject constructor(
-    private val gptApi: GptApi
+    private val gptApi: GptApi,
+    private val noteService: NoteService
 ): NoteRemoteDataSource {
     override suspend fun generateNote(request: GptRequest): Result<GptResponse> {
         return gptApi.requestNoteGenerate(request)
+    }
+
+    override suspend fun insertNote(noteDto: NoteDto) {
+        return noteService.insertNote(noteDto)
     }
 }
