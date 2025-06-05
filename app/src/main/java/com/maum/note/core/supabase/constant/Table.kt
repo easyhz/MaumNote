1b4ba1a25487af20e15fcdc2a6f110f567edcfb2
easyhz <jiton3@naver.com>
2025-06-05 12:36:16 +0900

    feat: Table.NOTES 추가

--- app/src/main/java/com/maum/note/core/supabase/constant/Table.kt
+++ app/src/main/java/com/maum/note/core/supabase/constant/Table.kt
@@ -13,7 +13,14 @@ sealed class Table(val name: String) {
     object NOTES : Table("NOTES") {
         const val ID = "id"
         const val USER_ID = "user_id"
+        const val STUDENT_ID = "student_id"
+        const val NOTE_TYPE = "note_type"
+        const val STUDENT_AGE = "student_age"
+        const val SENTENCE_COUNT = "sentence_count"
+        const val INPUT_CONTENT = "input_content"
+        const val RESULT = "result"
         const val CREATION_TIME = "creation_time"
+        const val UPDATED_TIME = "updated_time"
         const val IS_DELETED = "is_deleted"
     }
 }
