a5125765ec9de91fa35ab96d74a86069ebf4a156
easyhz <jiton3@naver.com>
2025-07-14 23:31:52 +0900

    feat: 색상 추가

--- app/src/main/java/com/maum/note/ui/theme/Color.kt
+++ app/src/main/java/com/maum/note/ui/theme/Color.kt
@@ -35,3 +35,9 @@ val DestructiveRed = Color(0xFFFF3D3D)
 
 @Stable
 val ButtonShapeColor = Color(0xFFF0F0F2)
+
+@Stable
+val FilledIconPrimary = Color(0xFF7EC1FF)
+
+@Stable
+val FilledIconDisabled = Color(0xFFE1E1E1)
