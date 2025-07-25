85375aa480011895e1b658ba80e84e31b1a5b2c1
easyhz <jiton3@naver.com>
2025-07-25 21:07:58 +0900

    feat: CreatePostUseCase 추가

---
+++ app/src/main/java/com/maum/note/domain/board/usecase/CreatePostUseCase.kt
@@ -0,0 +1,14 @@
+package com.maum.note.domain.board.usecase
+
+import com.maum.note.core.common.base.BaseUseCase
+import com.maum.note.domain.board.model.request.CreatePostRequest
+import com.maum.note.domain.board.repository.BoardRepository
+import javax.inject.Inject
+
+class CreatePostUseCase @Inject constructor(
+    private val boardRepository: BoardRepository
+): BaseUseCase<CreatePostRequest, Unit>() {
+    override suspend fun invoke(param: CreatePostRequest): Result<Unit> {
+        return boardRepository.createPost(param)
+    }
+}
