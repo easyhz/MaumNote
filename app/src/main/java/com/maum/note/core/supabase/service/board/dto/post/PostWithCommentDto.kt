7f115503ab8b7d7f76abd256d8536b5ac03a38a9
easyhz <jiton3@naver.com>
2025-07-25 22:19:53 +0900

    feat: PostWithCommentDto 추가

---
+++ app/src/main/java/com/maum/note/core/supabase/service/board/dto/post/PostWithCommentDto.kt
@@ -0,0 +1,34 @@
+package com.maum.note.core.supabase.service.board.dto.post
+
+import com.maum.note.core.supabase.constant.Table
+import com.maum.note.core.supabase.service.user.dto.UserDto
+import kotlinx.datetime.Instant
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PostWithCommentDto(
+    @SerialName(Table.POSTS.ID)
+    val id: String,
+    @SerialName(Table.POSTS.USER_ID)
+    val userId: String,
+    @SerialName(Table.POSTS.TITLE)
+    val title: String,
+    @SerialName(Table.POSTS.CONTENT)
+    val content: String,
+    @SerialName(Table.POSTS.IS_ANONYMOUS)
+    val isAnonymous: Boolean,
+    @SerialName(Table.POSTS.CREATED_AT)
+    val createdAt: Instant,
+    @SerialName(Table.POSTS.IS_DELETED)
+    val isDeleted: Boolean = false,
+    @SerialName(Table.USERS.DTO_NAME)
+    val user: UserDto,
+    @SerialName(Table.POSTS.COMMENT_COUNT)
+    val commentCount: List<CommentCountWrapper>
+)
+
+@Serializable
+data class CommentCountWrapper(
+    val count: Int
+)
