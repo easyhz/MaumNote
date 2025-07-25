f10e536908b1a565800329e2b6d7d7acd6b3ac30
easyhz <jiton3@naver.com>
2025-07-25 22:21:33 +0900

    feat: PostService fetchPosts 추가

--- app/src/main/java/com/maum/note/core/supabase/service/board/service/post/PostService.kt
+++ app/src/main/java/com/maum/note/core/supabase/service/board/service/post/PostService.kt
@@ -1,8 +1,10 @@
 package com.maum.note.core.supabase.service.board.service.post
 
 import com.maum.note.core.supabase.service.board.dto.post.PostDto
+import com.maum.note.core.supabase.service.board.dto.post.PostWithCommentDto
 
 interface PostService {
-
     suspend fun insertPost(post: PostDto)
+
+    suspend fun fetchPosts(): List<PostWithCommentDto>
 }
--- app/src/main/java/com/maum/note/core/supabase/service/board/service/post/PostServiceImpl.kt
+++ app/src/main/java/com/maum/note/core/supabase/service/board/service/post/PostServiceImpl.kt
@@ -2,13 +2,47 @@ package com.maum.note.core.supabase.service.board.service.post
 
 import com.maum.note.core.supabase.constant.Table
 import com.maum.note.core.supabase.service.board.dto.post.PostDto
+import com.maum.note.core.supabase.service.board.dto.post.PostWithCommentDto
 import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.query.Columns
+import io.github.jan.supabase.postgrest.query.Order
 import javax.inject.Inject
 
 class PostServiceImpl @Inject constructor(
     private val postgrest: Postgrest
-): PostService {
+) : PostService {
     override suspend fun insertPost(postDto: PostDto) {
         postgrest.from(Table.POSTS.name).insert(postDto)
+    }
+
+    override suspend fun fetchPosts(): List<PostWithCommentDto> {
+        return postgrest.from("POSTS").select(
+            columns = Columns.raw(
+                """
+                    ${Table.POSTS.ID},
+                    ${Table.POSTS.USER_ID},
+                    ${Table.POSTS.TITLE},
+                    ${Table.POSTS.CONTENT},
+                    ${Table.POSTS.IS_ANONYMOUS},
+                    ${Table.POSTS.IS_DELETED},
+                    ${Table.POSTS.CREATED_AT},
+                    ${Table.USERS.DTO_NAME}:${Table.USERS.ID_DTO_NAME} (
+                        ${Table.USERS.ID},
+                        ${Table.USERS.NICKNAME},
+                        ${Table.USERS.STUDENT_AGE},
+                        ${Table.USERS.FCM_TOKEN},
+                        ${Table.USERS.IS_DELETED},
+                        ${Table.USERS.HAS_AGREED_TO_TERMS}
+                    ),
+                    ${Table.POSTS.COMMENT_COUNT}: ${Table.COMMENTS.name}(count)
+                """.trimIndent()
+            ), request = {
+                filter {
+                    eq(Table.POSTS.IS_DELETED, false)
+                }
+                order(Table.POSTS.CREATED_AT, Order.DESCENDING)
+                range(0, 10)
+            }
+        ).decodeList<PostWithCommentDto>()
     }
 }
