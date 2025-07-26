00c07c744b48fe653695a5a71fcc79f9c4057e9b
easyhz <jiton3@naver.com>
2025-07-26 21:40:42 +0900

    feat: FullLoadingIndicator 추가

---
+++ app/src/main/java/com/maum/note/core/designSystem/component/loading/FullLoadingIndicator.kt
@@ -0,0 +1,93 @@
+package com.maum.note.core.designSystem.component.loading
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import com.airbnb.lottie.LottieProperty
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.airbnb.lottie.compose.rememberLottieDynamicProperties
+import com.airbnb.lottie.compose.rememberLottieDynamicProperty
+import com.maum.note.R
+import com.maum.note.core.designSystem.extension.modifier.noRippleClickable
+import com.maum.note.ui.theme.MainBackground
+import com.maum.note.ui.theme.Primary
+
+@Composable
+fun FullLoadingIndicator(
+    modifier: Modifier = Modifier,
+    isLoading: Boolean = false,
+    width: Dp = (LocalConfiguration.current.screenWidthDp / 4).dp,
+    height: Dp = (LocalConfiguration.current.screenWidthDp / 4).dp,
+) {
+    val focusManager = LocalFocusManager.current
+
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
+    val  dynamicProperties  = rememberLottieDynamicProperties(
+        rememberLottieDynamicProperty(
+            property = LottieProperty.COLOR_FILTER,
+            value = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
+                Primary.hashCode(),
+                BlendModeCompat.SRC_ATOP
+            ),
+            keyPath = arrayOf( "**" )
+        )
+    )
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = isLoading
+    )
+
+    LaunchedEffect(isLoading) {
+        focusManager.clearFocus()
+    }
+    AnimatedVisibility(
+        visible = isLoading,
+        enter = fadeIn(),
+        exit = fadeOut()
+    ) {
+        Box(
+            modifier = modifier.fillMaxSize().noRippleClickable {  }.background(MainBackground.copy(0.5f)),
+            contentAlignment = Alignment.Center
+        ) {
+            LottieAnimation(
+                modifier = modifier
+                    .width(width)
+                    .height(height),
+                composition = composition,
+                progress = { progress },
+                dynamicProperties = dynamicProperties,
+                contentScale = ContentScale.FillWidth
+            )
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun FullLoadingIndicatorPreview() {
+    FullLoadingIndicator(isLoading = true)
+}
