4d3b68dc7d86a5ec56e3582fed4094437a45ef51
easyhz <jiton3@naver.com>
2025-07-26 23:11:57 +0900

    feat: IS_ANONYMOUS 키 추가

--- app/src/main/java/com/maum/note/core/datastore/constant/PreferencesKey.kt
+++ app/src/main/java/com/maum/note/core/datastore/constant/PreferencesKey.kt
@@ -4,4 +4,6 @@ object PreferencesKey {
     const val AGE = "age"
     const val NOTIFICATION_STATUS = "notification_status"
     const val NOTIFICATION_DATE = "notification_date"
+
+    const val IS_ANONYMOUS = "is_anonymous"
 }
