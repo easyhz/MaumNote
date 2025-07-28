85e6ce8bea566ac1695f06e1b1b441dee09d9d2a
easyhz <jiton3@naver.com>
2025-07-28 23:57:54 +0900

    feat: UserStep NeedSynchronize 추가

--- app/src/main/java/com/maum/note/core/model/user/UserStep.kt
+++ app/src/main/java/com/maum/note/core/model/user/UserStep.kt
@@ -4,6 +4,7 @@ sealed class UserStep {
     data object NewUserToOnboarding : UserStep()
     data object ExistingUserToOnboarding : UserStep()
     data object AlreadyLoginToMain : UserStep()
+    data object NeedSynchronize : UserStep()
     data class Maintenance(val notice: String) : UserStep()
     data class Update(val newVersion: String) : UserStep()
 }
