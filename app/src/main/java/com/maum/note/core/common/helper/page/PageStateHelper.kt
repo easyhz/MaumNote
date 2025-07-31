14dec1e5cd6d7d6e9e40045e5d89fee4b8129505
easyhz <jiton3@naver.com>
2025-07-31 10:37:16 +0900

    feat: PageStateHelper.kt 추가

---
+++ app/src/main/java/com/maum/note/core/common/helper/page/PageStateHelper.kt
@@ -0,0 +1,28 @@
+package com.maum.note.core.common.helper.page
+
+import androidx.paging.LoadState
+import androidx.paging.compose.LazyPagingItems
+
+
+val LazyPagingItems<*>.isInitialLoading: Boolean
+    get() = loadState.refresh is LoadState.Loading
+
+val LazyPagingItems<*>.isAppending: Boolean
+    get() = loadState.append is LoadState.Loading
+
+val LazyPagingItems<*>.isError: Boolean
+    get() = loadState.refresh is LoadState.Error || loadState.append is LoadState.Error
+
+val LazyPagingItems<*>.isInitialError: Boolean
+    get() = loadState.refresh is LoadState.Error
+
+val LazyPagingItems<*>.isAppendError: Boolean
+    get() = loadState.append is LoadState.Error
+
+val LazyPagingItems<*>.isEmpty: Boolean
+    get() = itemCount == 0 &&
+            loadState.refresh is LoadState.NotLoading &&
+            loadState.append is LoadState.NotLoading
+
+val LazyPagingItems<*>.hasMoreToLoad: Boolean
+    get() = loadState.append.endOfPaginationReached.not()
