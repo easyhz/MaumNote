4653b9ca3f47198ff0bea481818d321660f84f95
easyhz <jiton3@naver.com>
2025-08-07 08:17:12 +0900

    chore: versionCode 7 versionName 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.maum.note"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.2"
+        versionCode = 7
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
